5be664c8698ceebe5bb595930d6cc92fd04745dd
ghayas110 <team@logochemist.com>
2022-11-11 08:11:09 +0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import DarkTheme from '../layouts/Dark'
+import Demos from "./demos";
+import Homepage2 from './home/home2-dark'
+export default function Home() {
+  return (
+    <DarkTheme>
+   <Homepage2/>
+    </DarkTheme>
+  )
+}
