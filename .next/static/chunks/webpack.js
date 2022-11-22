8f5982429466fc65a22bcc28fe53cdfc644bcf8b
HamzaWaseem855 <hamza.waseem@techexons.com>
2022-11-22 23:03:52 +0500

    has add picture of ceo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2017529eb6a3d4cd"; }
+/******/ 		__webpack_require__.h = function() { return "51f15c4cc3a97dc6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
