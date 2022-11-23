ab7d4610aa4c15252fdc56267600796d315b06a3
ghayas110 <team@logochemist.com>
2022-11-23 19:27:06 +0500

    Ghays

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "11108b69ddeca87c"; }
+/******/ 		__webpack_require__.h = function() { return "ea03d89a14fb35e1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,11 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="Agha"
+            content="SSSSCO"
           />
           <meta
             name="description"
-            content="Agha"
+            content="SSSSCO"
           />
           <meta name="author" content="" />
           <link rel="shortcut icon" href="/img/favicon.ico" />
