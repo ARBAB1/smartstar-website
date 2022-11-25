1239647f8fc32158c5b51f81139b88492d76f85e
HamzaWaseem855 <hamza.waseem@techexons.com>
2022-11-25 22:51:08 +0500

    Responsiveness of image loader

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b184176e6ee385d"; }
+/******/ 		__webpack_require__.h = function() { return "6a0e90c524b9636f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
